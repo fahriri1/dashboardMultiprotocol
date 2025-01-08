fa12bb5bfa357d3d68800d3ef614365eef7b76ff
fahriri1 <fahririzkis1004@gmail.com>
2025-01-08 13:09:19 +0700

    add server run flask

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
