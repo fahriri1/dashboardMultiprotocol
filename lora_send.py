c3faab0eea770fc32dea98182fb60195e0411e4c
fahriri1 <fahririzkis1004@gmail.com>
2025-05-11 16:28:23 +0700

    lora-fix

---
+++ lora_send.py
@@ -0,0 +1,17 @@
+import serial
+
+serial_port_lora = "/dev/ttyS0"
+baud_rate = 9600
+
+try:
+    lora = serial.Serial(serial_port_lora, baud_rate, timeout=1)
+    print("LoRa E220 Initialized")
+except Exception as e:
+    print(f"Error: {e}")
+    exit()
+
+status_lora = True
+while status_lora:
+    if lora.in_waiting > 0:
+        received = lora.readline().decode().strip()
+        print(f"Received: {received}")
