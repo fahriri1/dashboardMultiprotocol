1228079f9e36b909a50e7eed4017fa53673b7861
fahriri1 <fahririzkis1004@gmail.com>
2025-05-11 16:32:59 +0700

    lora-fix

--- lora_send.py
+++ lora_send.py
@@ -10,6 +10,13 @@ except Exception as e:
     print(f"Error: {e}")
     exit()
 
+try:
+    data = "start"
+    lora.write(data.encode())  # Send data as bytes
+    print(f"Sent: {data}")
+except Exception as e:
+    print(f"Error sending data: {e}")
+
 status_lora = True
 while status_lora:
     if lora.in_waiting > 0:
